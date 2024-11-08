3a02af277774a8578a847aac86e5a17770199599
Venkat Rohit Vemuri <70214314+vrohitv@users.noreply.github.com>
2024-11-08 14:39:10 -0800

    Update supaClient.js

--- topic/supaClient.js
+++ topic/supaClient.js
@@ -1,5 +1,5 @@
-var supaBaseUrl = "https://ubimrawvzdznjyqqshaf.supabase.co"
-var supaBaseAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViaW1yYXd2emR6bmp5cXFzaGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzNzI3OTksImV4cCI6MjAzNzk0ODc5OX0.vHSMo4EO_tcauDHvzyrEnB-pOs0O_Nbp1DsGYKVFb2E"
+var supaBaseUrl = ""
+var supaBaseAPIKey = ""
 const { createClient } = require('@supabase/supabase-js');
 
 exports.supabase = createClient(supaBaseUrl, supaBaseAPIKey)
