5b95659e7abbc1a75ba5598e4e10d1945a3d964f
Venkat Rohit Vemuri <70214314+vrohitv@users.noreply.github.com>
2024-11-08 14:39:51 -0800

    Update supa.js

--- auth/supa.js
+++ auth/supa.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
-var supaBaseUrl = "https://ubimrawvzdznjyqqshaf.supabase.co"
-var supaBaseAPIKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InViaW1yYXd2emR6bmp5cXFzaGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIzNzI3OTksImV4cCI6MjAzNzk0ODc5OX0.vHSMo4EO_tcauDHvzyrEnB-pOs0O_Nbp1DsGYKVFb2E"
+var supaBaseUrl = ""
+var supaBaseAPIKey = ""
 const supabase = createClient(supaBaseUrl, supaBaseAPIKey)
 const { v4: uuidv4 } = require('uuid');
 
